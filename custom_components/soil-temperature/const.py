67cf81b228ac79c96dab0de005f5c6e150b74499
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2025-09-02 10:30:46 +0100

    Update const.py

--- custom_components/soil-temperature/const.py
+++ custom_components/soil-temperature/const.py
@@ -1,30 +1,23 @@
-"""Constants for the Soil Temperature integration."""
-
 DOMAIN = "soil_temperature"
 
-# Configuration
 CONF_UNIT_SYSTEM = "unit_system"
 UNIT_SYSTEM_IMPERIAL = "imperial"
 UNIT_SYSTEM_METRIC = "metric"
 DEFAULT_UNIT_SYSTEM = UNIT_SYSTEM_IMPERIAL
 
-# Configuration Defaults
 CONF_LATITUDE = "latitude"
 CONF_LONGITUDE = "longitude"
 DEFAULT_SCAN_INTERVAL = 60  # minutes
 
-# API
 API_ENDPOINT = "https://soiltemperature.app/api/weatherTimeline?lat={lat}&lng={lng}"
 GEOCODE_API_ENDPOINT = "https://soiltemperature.app/api/geocodeReverse?lat={lat}&lng={lng}"
 
-# Sensor Information
 SENSOR_TEMPERATURE_NAME = "Soil Temperature"
 SENSOR_MOISTURE_NAME = "Soil Moisture"
 SENSOR_UNIT_TEMPERATURE_IMPERIAL = "°F"
 SENSOR_UNIT_TEMPERATURE_METRIC = "°C"
 SENSOR_UNIT_MOISTURE = "m³/m³"
 
-# Data Keys from API
 SOIL_TEMPERATURE_KEYS = [
     "soil_temperature_0cm",
     "soil_temperature_6cm",
@@ -42,5 +35,4 @@ SOIL_MOISTURE_KEYS = [
     "soil_moisture_27_81cm",
 ]
 
-# Summary types
 SUMMARY_TYPES = ["max", "min", "mean"]
